647bc8a473d3088a8cb917d7e2452cae58b5b4e1
yatharth.mathur <yatharth.mathur@accops.com>
2021-10-06 18:50:28 +0530

    package config

---
+++ README.md
@@ -0,0 +1,1 @@
+Python package to convert CSV file into formatted PDFs.
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='csv_to_pdf',  
+     version='0.1',
+     scripts=['csv_to_pdf'] ,
+     author="Yatharth Mathur",
+     author_email="yatharth.mathur@gmail.com",
+     description="CSV to formatted PDF converter.",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/accops-yatharth/csv-to-pdf",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
